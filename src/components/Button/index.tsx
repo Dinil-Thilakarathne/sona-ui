aebb0a00ae977b58341351216bcfc51b53efa4ed
dinil-thilakarathne <sonacode44@gmail.com>
2025-04-13 01:29:37 +0530

    feat: update Button component to support link rendering and improve default styling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import Link from "next/link";
 
 import { cn } from "@/libs/utils";
 
@@ -8,22 +9,35 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className?: string;
   variant?: "default" | "outline" | "filled";
+  link?: string;
 }
 
 export default function Button({
   children,
   className,
   variant = "default",
+  link,
   ...props
 }: ButtonProps) {
   const baseClasses =
     "px-4 py-2 rounded-full text-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 hover:cursor-pointer transition duration-200 ease-in-out";
 
   const variantClasses = {
-    default: "bg-gray-200 text-black hover:bg-gray-300",
+    default: "bg-white text-black hover:bg-slate-100",
     outline: "border border-gray-300 text-black hover:bg-gray-100",
     filled: "bg-blue-500 text-white hover:bg-blue-600",
   };
+
+  if (link) {
+    return (
+      <Link
+        href={link}
+        className={cn(baseClasses, variantClasses[variant], className)}
+      >
+        {children}
+      </Link>
+    );
+  }
 
   return (
     <button
