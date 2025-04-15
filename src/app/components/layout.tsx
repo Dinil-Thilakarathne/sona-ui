57f2a18192f376d25fe070d3ef83fbc65eca5c7c
dinil-thilakarathne <sonacode44@gmail.com>
2025-04-15 23:31:45 +0530

    feat: update sidebar items to use ComponentItems for consistency

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Sidebar from "@/components/ComponentsSidebar";
-import { ComponentSidebarItems } from "@/libs/data";
+import { ComponentItems } from "@/libs/data";
 
 const ComponentLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <div className="container mx-auto flex min-h-[calc(100vh-75px)]">
-      <Sidebar title="Components" items={ComponentSidebarItems} />
-      <main className="flex-1 p-2 md:p-6">{children}</main>
+      <Sidebar title="Components" items={ComponentItems} />
+      <main className="w-full flex-1 p-2 md:p-6">{children}</main>
     </div>
   );
 };
