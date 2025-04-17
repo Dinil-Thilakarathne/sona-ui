84c0960c125b622027e9a01d0b8f60aec2df0387
dinil-thilakarathne <sonacode44@gmail.com>
2025-04-17 11:49:56 +0530

    feat: add Feedback link to navigation

--- src/libs/data.ts
+++ src/libs/data.ts
@@ -3,6 +3,10 @@ import { ComponentItemsProps, navLinksProps } from "./types";
 export const navLinks: navLinksProps[] = [
   { name: "Docs", href: "/docs", tag: "soon" },
   { name: "Components", href: "/components" },
+  {
+    name: "Feedback",
+    href: "https://sonacode.notion.site/1d8b84f1ce5580f38f30ec04e9db9d78?pvs=105",
+  },
 ];
 
 export const ComponentItems: ComponentItemsProps[] = [
