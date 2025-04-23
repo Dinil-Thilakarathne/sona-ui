b7e7b19319607c529a1f71974b9354ee5eee4a92
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-04-23 12:57:55 +0530

    feat: add SpinningText component with animation and example usage

---
+++ src/app/components/spinningText/page.tsx
@@ -0,0 +1,44 @@
+import ComponentWrapper from "@/components/Common/ComponentWrapper";
+import { SpinningText } from "./SpinningText";
+import { CodeSyntaxHighlighter } from "@/components/Common/CodeSyntaxHighlighter";
+import Tabs from "@/components/Common/Tabs";
+
+const Page = () => {
+        const tabsData = [
+        {
+          title: "Page.tsx",
+          content: (
+            <CodeSyntaxHighlighter
+              filePath="components/spinningText/page.txt"
+              language="tsx"
+              className=""
+            />
+          ),
+        },
+        {
+          title: "RippleButton.tsx",
+          content: (
+            <CodeSyntaxHighlighter
+              filePath="components/spinningText/SpinningText.txt"
+              language="tsx"
+              className=""
+            />
+          ),
+        },
+        ]
+  return (
+    <main className="space-y-8">
+      <div>
+        <ComponentWrapper>
+          <div className="flex min-h-[320px] items-center justify-center">
+            <SpinningText>This is example text!</SpinningText>
+          </div>
+        </ComponentWrapper>
+      </div>
+
+      <Tabs tabs={tabsData} className="" />
+    </main>
+  );
+};
+
+export default Page;
