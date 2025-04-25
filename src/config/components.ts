78c6b591007d66ec55251399244024c19b508f63
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-04-25 21:33:25 +0530

    feat: add component navigation links and site metadata configuration

---
+++ src/config/components.ts
@@ -0,0 +1,26 @@
+import { ComponentItemsProps } from "@/lib/types";
+
+export const componentNavigationLinks: ComponentItemsProps[] = [
+  {
+    name: "Accordion",
+    slug: "accordion",
+    href: "/docs/accordion",
+  },
+  {
+    name: "Ripple Button",
+    slug: "rippleButton",
+    href: "/docs/rippleButton",
+  },
+  {
+    name: "Spinning Text",
+    tag: "new",
+    slug: "spinningText",
+    href: "/docs/spinningText",
+  },
+  {
+    name: "Stagger Text",
+    tag: "new",
+    slug: "staggerText",
+    href: "/docs/staggerText",
+  },
+];
