11002b0a8e9e21226be9625e270931738919e199
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-04-25 21:58:46 +0530

    fix: update Docs navigation link to point to the home page

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,7 @@
 import { navLinksProps } from "./types";
 
 export const navLinks: navLinksProps[] = [
-  { name: "Docs", href: "/docs", tag: "new" },
+  { name: "Docs", href: "/docs/home", tag: "new" },
   {
     name: "Feedback",
     href: "https://sonacode.notion.site/1d8b84f1ce5580f38f30ec04e9db9d78?pvs=105",
