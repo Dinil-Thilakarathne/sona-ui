3a2c17eddd89e173754c0652c41df2f6ddf37c4b
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-08 16:19:46 +0530

    refactor: restructure LinkPreview example component with improved layout and content

--- src/registry/example/LinkPreview_ex.tsx
+++ src/registry/example/LinkPreview_ex.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import LinkPreview from '../sonaui/linkPreview/LinkPreview'
+import React from "react";
+import LinkPreview from "../sonaui/linkPreview/LinkPreview";
+import ComponentWrapper from "@/components/Common/ComponentWrapper";
 
 const LinkPreview_ex = () => {
   return (
     <div>
-        <div className='border p-8'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, ullam.
-            <LinkPreview link='https://www.google.com' text='Google' className='flex h-full w-full items-center justify-center' />
+      <ComponentWrapper>
+        <div className="flex w-full flex-col justify-center gap-4">
+          <h3>Projects that I works on!!!</h3>
+          <ul className=" list-disc *:ml-2 *:lg:ml-4">
+            <li>
+              Sona UI - Open Source UI Component Library :{" "}
+              <LinkPreview
+                link="https://sona-ui.vercel.app/"
+                text="Sona UI"
+                className="flex h-full w-full items-center justify-center"
+              />
+            </li>
+          </ul>
         </div>
+      </ComponentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default LinkPreview_ex
+export default LinkPreview_ex;
