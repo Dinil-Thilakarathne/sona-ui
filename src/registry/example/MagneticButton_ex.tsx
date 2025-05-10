8f5f555b172819a3e558329ab1cefc1292f0b8fc
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-11 01:04:49 +0530

    refactor: update button text color for improved contrast in MagneticButton example

--- src/registry/example/MagneticButton_ex.tsx
+++ src/registry/example/MagneticButton_ex.tsx
@@ -8,7 +8,7 @@ const Magnetic_ex = () => {
     <ComponentWrapper>
       <div className="rounded-full border border-dashed border-slate-400 p-2">
         <Magnetic interactionArea="parent">
-          <button className="cursor-pointer rounded-full bg-slate-400 dark:bg-slate-600 px-6 py-4 font-semibold text-white">
+          <button className="cursor-pointer rounded-full bg-slate-400 px-6 py-4 font-semibold text-slate-800 dark:bg-slate-600 dark:text-slate-50">
             Magnetic Button
           </button>
         </Magnetic>
