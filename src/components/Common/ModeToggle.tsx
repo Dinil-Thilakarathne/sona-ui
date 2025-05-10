222136833ac74b37dfaf43ec4a26dcef13b81f7b
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-10 23:33:58 +0530

    refactor: enhance accessibility by adding aria-labels and type attributes to buttons in ModeToggle component

--- src/components/Common/ModeToggle.tsx
+++ src/components/Common/ModeToggle.tsx
@@ -9,13 +9,14 @@ import { cn } from "@/lib/utils";
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
 
-
   return (
     <div className="overflow-clip rounded-2xl border border-slate-800 dark:border-slate-50">
       <div className="flex items-center justify-center space-x-0.5 p-0.5 *:text-slate-800 dark:*:text-slate-50">
         <button
           className="cursor-pointer overflow-clip"
           onClick={() => setTheme("dark")}
+          aria-label="Toggle dark mode"
+          type="button"
         >
           <Moon
             className={cn(
@@ -26,7 +27,12 @@ export function ModeToggle() {
             )}
           />
         </button>
-        <button className="cursor-pointer" onClick={() => setTheme("light")}>
+        <button
+          className="cursor-pointer"
+          onClick={() => setTheme("light")}
+          aria-label="Toggle light mode"
+          type="button"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rounded-full p-0.5 opacity-75 hover:opacity-100" />
         </button>
         <span className="sr-only">Toggle theme</span>
