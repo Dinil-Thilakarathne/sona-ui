9527a325642b959574fe48d2ff43eed21b1bef41
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-11 22:36:07 +0530

    feat: implement dynamic star count display using NumberFlow component

--- src/components/Common/StartCount.tsx
+++ src/components/Common/StartCount.tsx
@@ -1,20 +1,28 @@
 "use client";
 
-import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 
 import { useGitStars } from "@/hooks/useGitStars";
+import { useEffect, useState } from "react";
+import NumberFlow from "@number-flow/react";
 
 const StartCount = () => {
+  const [starCount, setStarCount] = useState<number>(0o0);
   const { stars } = useGitStars("Dinil-Thilakarathne", "sona-ui");
+
+  useEffect(() => {
+    if (stars) {
+      setStarCount(stars);
+    }
+  }, [stars]);
 
   return (
     <span className="group flex items-center justify-center space-x-1.5 text-sm">
       <FaGithub />
       <span className="leading-[14px]">Star on GitHub</span>
       <FaStar className="mb-0.5 text-[14px] group-hover:text-yellow-400" />
-      <span className="leading-[14px]">0{stars}</span>
+      <NumberFlow value={starCount} />
     </span>
   );
 };
