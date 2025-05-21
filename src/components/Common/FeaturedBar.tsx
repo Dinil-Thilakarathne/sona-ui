1b2d24c2fbfcbfbd634cba78ed2c371b0abc21e3
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-21 23:33:23 +0530

    feat: add FeaturedBar component with example text

---
+++ src/components/Common/FeaturedBar.tsx
@@ -0,0 +1,7 @@
+export default function FeaturedBar() {
+  return (
+    <div className="flex w-full items-center justify-center py-2 dark:bg-slate-800">
+      <h1>this is example text</h1>
+    </div>
+  );
+}
