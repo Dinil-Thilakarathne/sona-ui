feef8603d1b5681cdc844905364cbc46fc191530
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-31 07:56:36 +0530

    fix: add missing type properties to component navigation links

--- src/config/components.ts
+++ src/config/components.ts
@@ -6,48 +6,57 @@ export const componentNavigationLinks: ComponentItemsProps[] = [
     tag: "updated",
     slug: "accordion",
     href: "/docs/accordion",
+    type: "Components",
   },
   {
     name: "Ripple Button",
     slug: "rippleButton",
     href: "/docs/rippleButton",
+    type: "Components",
   },
   {
     name: "Spinning Text",
     slug: "spinningText",
     href: "/docs/spinningText",
+    type: "Text",
   },
   {
     name: "Stagger Text",
     slug: "staggerText",
     href: "/docs/staggerText",
+    type: "Text",
   },
   {
     name: "Vertical Tab",
     slug: "verticalTab",
     href: "/docs/verticalTab",
+    type: "Components",
   },
   {
     name: "Expandable Tabs",
     slug: "expandableTabs",
     href: "/docs/expandableTabs",
+    type: "Components",
   },
   {
     name: "Magnetic Button",
     slug: "magneticButton",
     href: "/docs/magneticButton",
+    type: "Effects",
   },
   {
     name: "Link Preview",
     tag: "updated",
     slug: "linkPreview",
     href: "/docs/linkPreview",
+    type: "Components",
   },
   {
     name: "Marquee",
     tag: "new",
     slug: "marquee",
     href: "/docs/marquee",
+    type: "Effects",
   },
 ];
 
