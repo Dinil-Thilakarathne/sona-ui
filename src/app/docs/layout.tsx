9db48b75894a4825741ff8b729f1abfc62f65b36
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-06-05 16:52:43 +0530

    fix: update layout structure to use grid for improved responsiveness

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -5,7 +5,7 @@ import { sortedComponentNavigationLinks } from "@/config/components";
 
 const ComponentLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <main className="flex min-h-[calc(100vh-75px)]">
+    <main className="grid min-h-[calc(100vh-75px) grid-cols-1 lg:grid-cols-[25%_1fr]">
       <Sidebar title="Components" items={sortedComponentNavigationLinks} />
       <section className="w-full flex-1 p-2 md:p-6">{children}</section>
     </main>
