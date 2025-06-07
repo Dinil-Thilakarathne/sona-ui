db21b570576cc1679c6ee7fcf37c924d987b06d2
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-06-07 23:38:49 +0530

    fix: update Sidebar component usage and improve layout structure

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,12 +1,11 @@
 import { type ReactNode } from "react";
 
 import Sidebar from "@/components/ComponentsSidebar";
-import { sortedComponentNavigationLinks } from "@/config/components";
 
 const ComponentLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <main className="grid min-h-[calc(100vh-75px) grid-cols-1 lg:grid-cols-[25%_1fr]">
-      <Sidebar title="Components" items={sortedComponentNavigationLinks} />
+    <main className="min-h-[calc(100vh-75px) grid grid-cols-1 lg:grid-cols-[25%_1fr] xl:grid-cols-[var(--sidebar-width)_1fr]">
+      <Sidebar />
       <section className="w-full flex-1 p-2 md:p-6">{children}</section>
     </main>
   );
