87874dbdb358b72100bea4793cf85d92bb26280f
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-06-23 16:04:31 +0530

    feat: add example usage for BubbleUpButton component

---
+++ src/registry/example/BubbleUpButton_ex.tsx
@@ -0,0 +1,10 @@
+import ComponentWrapper from "@/components/Common/ComponentWrapper";
+import BubbleUpButton from "../sonaui/bubbleUpButton/BubbleUpButton";
+
+export default function BubbleUpButtonExample() {
+  return (
+    <ComponentWrapper className="!py-8">
+      <BubbleUpButton className="bg-background">Hover me!</BubbleUpButton>
+    </ComponentWrapper>
+  );
+}
