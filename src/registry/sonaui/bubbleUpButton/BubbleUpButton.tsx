86984ddbf2a47eaa27383c994ca68a336bf670ed
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-06-23 11:14:55 +0530

    feat: enhance BubbleUpButton component with props for children and motion controls

--- src/registry/sonaui/bubbleUpButton/BubbleUpButton.tsx
+++ src/registry/sonaui/bubbleUpButton/BubbleUpButton.tsx
@@ -1,25 +1,36 @@
 "use client";
 
-import { motion, useAnimation } from "motion/react";
+import type { ReactNode } from "react";
+import {
+  motion,
+  MotionConfig,
+  MotionConfigProps,
+  useAnimation,
+} from "motion/react";
 
-export default function BubbleUpButton() {
+interface BubbleUpButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: ReactNode;
+  motionControls?: MotionConfigProps;
+}
+export default function BubbleUpButton({
+  children,
+  motionControls = {
+    transition: { duration: 2, type: "spring", stiffness: 300, damping: 40 },
+  },
+  ...props
+}: BubbleUpButtonProps) {
   const controls = useAnimation();
 
   const handleMouseEnter = async () => {
-    console.log("Mouse entered");
     await controls.start({
-      // scale: 1.1,
       clipPath: "ellipse(120% 120% at 50% 100%)",
-      transition: { type: "spring", stiffness: 300, damping: 40, duration: 2 },
     });
   };
 
   const handleMouseLeave = async () => {
-    console.log("Mouse left");
     await controls.start({
-      // scale: 0,
       clipPath: "ellipse(120% 120% at 50% -120%)",
-      transition: { type: "spring", stiffness: 300, damping: 40, duration: 2 },
     });
     controls.set({ clipPath: "ellipse(0% 0% at 50% 100%)" });
   };
@@ -28,15 +39,19 @@ export default function BubbleUpButton() {
     <button
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="relative flex h-fit w-fit overflow-clip rounded-2xl border bg-black px-16 py-2"
+      className="relative flex h-fit w-fit cursor-pointer overflow-clip rounded-2xl border bg-black px-16 py-2"
+      {...props}
     >
-      <motion.div
-        animate={controls}
-        initial={{ scale: 1, clipPath: "ellipse(0% 0% at 50% 100%)" }}
-        className="absolute top-0 left-0 h-full w-full bg-white"
-        transition={{ duration: 2 }}
-      />
-      <span className="relative text-white mix-blend-difference">text</span>
+      <MotionConfig {...motionControls}>
+        <motion.div
+          animate={controls}
+          initial={{ scale: 1, clipPath: "ellipse(0% 0% at 50% 100%)" }}
+          className="absolute top-0 left-0 h-full w-full bg-white"
+        />
+      </MotionConfig>
+      <span className="relative text-white mix-blend-difference">
+        {children}
+      </span>
     </button>
   );
 }
