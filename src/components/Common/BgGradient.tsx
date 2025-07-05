508e999703bf88d12e17aa69829d0cd04d299f0a
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-07-05 15:06:46 +0530

    feat: add BgGradient component and integrate it into Hero section

---
+++ src/components/Common/BgGradient.tsx
@@ -0,0 +1,5 @@
+export default function BgGradient() {
+  return (
+    <div className="bg-gradient pointer-events-none fixed top-0 -left-[25] z-0 min-h-screen w-[125vw] rounded-full opacity-75 blur-3xl" />
+  );
+}
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/assets/svgs";
 import HeroGrid from "./HeroGrid";
 import AnimatedLink from "../Common/AnimatedLink";
+import BgGradient from "../Common/BgGradient";
 
 const TECH_STACK = [
   {
@@ -34,11 +35,12 @@ const Hero = () => {
   return (
     // mobile sidebar
     <section className="relative flex min-h-[calc(100vh-75px)] flex-col overflow-clip">
+      <BgGradient />
       <div className="absolute top-0 left-0 lg:hidden">
-        <Sidebar  />
+        <Sidebar />
       </div>
 
-      <div className="px-4 grid h-full grow items-center space-y-4 lg:grid-cols-[45%_1fr] lg:flex-row lg:space-y-0 lg:space-x-8">
+      <div className="relative grid h-full grow items-center space-y-4 px-4 lg:grid-cols-[45%_1fr] lg:flex-row lg:space-y-0 lg:space-x-8">
         <div className="flex h-fit flex-col gap-y-6">
           <div className="flex w-fit items-center justify-center space-x-2 rounded-full border-slate-800 bg-slate-50 px-4 py-2 dark:bg-slate-800">
             <heroContent.flag.icon aria-label="hero-flag-img" />
