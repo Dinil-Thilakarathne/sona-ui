4eba659348bb7d79c419b5bbb3ef615324351e07
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-11-17 11:59:26 +0530

    refactor: reorganize Hero component structure and update animations

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,89 +1,66 @@
-"use client";
+import Link from "next/link";
+import FadeInComp from "../Common/FadeIn";
+import ScrollUpButton from "../Common/ScrollUpButton";
+import SplitTextComp from "../Common/SplitTextComp";
 
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
-import { heroContent } from "@/lib/constants";
-import Sidebar from "../ComponentsSidebar";
-import BgGradient from "../Common/BgGradient";
-import FeatureGrid from "./FeatureGrid";
-import { useRef } from "react";
-
-gsap.registerPlugin(useGSAP);
-
-const Hero = () => {
-  const mainHeader = useRef<HTMLHeadingElement>(null);
-  const heroDescription = useRef<HTMLParagraphElement>(null);
-  const heroFlag = useRef<HTMLDivElement>(null);
-
-  useGSAP(() => {
-    const tl = gsap.timeline();
-    tl.from(mainHeader.current, {
-      y: 200,
-      opacity: 0.5,
-      duration: 0.5,
-    });
-    tl.from(
-      heroDescription.current,
-      {
-        y: 50,
-        opacity: 0.5,
-        duration: 0.25,
-      },
-      "-=0.15",
-    );
-    tl.from(
-      heroFlag.current,
-      {
-        y: 50,
-        opacity: 0.5,
-        duration: 0.25,
-      },
-      "-=0.15",
-    );
-  });
+export default function Hero() {
   return (
-    // mobile sidebar
-    <section className="relative flex min-h-[calc(100vh-75px)] flex-col overflow-clip">
-      <BgGradient />
-      <div className="absolute top-0 left-0 lg:hidden">
-        <Sidebar />
-      </div>
-
-      <div className="relative grid h-full grow items-center space-y-4 px-4 lg:flex-row lg:space-y-0 lg:space-x-8">
-        <div className="flex h-fit w-full flex-col items-center gap-y-6">
-          <div className="overflow-clip">
-            <div
-              className="flex w-fit items-center justify-center space-x-2 rounded-full border-slate-800 bg-slate-50 px-4 py-2 dark:bg-slate-800"
-              ref={heroFlag}
+    <section className="relative flex min-h-[calc(100vh-75px)] flex-col overflow-clip p-2 lg:p-4">
+      <div className="flex h-full grow flex-col justify-end gap-y-12">
+        <div className="flex min-h-[50vh] w-full flex-col justify-between md:min-h-[70vh]">
+          <div className="flex flex-col items-end gap-y-4">
+            <SplitTextComp
+              variant="lines"
+              maskType="lines"
+              animationProps={{
+                duration: 0.6,
+                rotate: 0,
+                filter: "blur(4px)",
+                opacity: 0,
+                yPercent: 40,
+              }}
             >
-              <heroContent.flag.icon aria-label="hero-flag-img" />
-              <span className="text-sm">{heroContent.flag.text}</span>
-            </div>
-          </div>
-          <div className="flex w-full flex-col items-center text-center">
-            <div className="overflow-clip">
-              <h1
-                className="font-clash-display _font-roboto text-7xl font-bold uppercase lg:text-9xl 2xl:text-[20rem]"
-                ref={mainHeader}
-              >
-                {heroContent.header}
-              </h1>
-            </div>
-            <div className="overflow-clip">
-              <p
-                className="max-w-2xl text-center text-lg text-gray-600 md:text-xl dark:text-gray-300"
-                ref={heroDescription}
-              >
-                {heroContent.description}
+              <p className="text-right text-balance md:max-w-[50vw]">
+                A modern UI component library built with React and TailwindCSS
+                to help you build beautiful and accessible web applications
+                faster.
               </p>
-            </div>
+            </SplitTextComp>
+            <FadeInComp
+              className="overflow-visible"
+              animationProps={{
+                duration: 0.8,
+                delay: 0.2,
+                rotate: 0,
+                filter: "blur(4px)",
+                yPercent: 40,
+                opacity: 0,
+              }}
+            >
+              <Link href="/docs/accordion" className="block">
+                <ScrollUpButton text="Browse components" />
+              </Link>
+            </FadeInComp>
           </div>
+          <SplitTextComp
+            variant="words, chars"
+            maskType="chars"
+            animationProps={{
+              duration: 0.6,
+              stagger: 0.1,
+              rotate: 4,
+              filter: "blur(4px)",
+              opacity: 0,
+              yPercent: 60,
+              ease: "power2.inOut",
+            }}
+          >
+            <h1 className="_text-5xl text-[20vw] leading-none uppercase">
+              Sona ui
+            </h1>
+          </SplitTextComp>
         </div>
-        <FeatureGrid />
       </div>
     </section>
   );
-};
-
-export default Hero;
+}
