8aa43c13a603303069de638f606e1d49e21efa01
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-11-17 15:41:20 +0530

    feat: implement DocsLayoutShell component and refactor ComponentLayout for improved structure

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,16 +1,9 @@
 import { type ReactNode } from "react";
 
-import Sidebar from "@/components/ComponentsSidebar";
+import DocsLayoutShell from "@/components/DocsLayoutShell";
 
 const ComponentLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <main className="min-h-[calc(100vh-75px) mt-header-height flex">
-      <Sidebar />
-      <section className="lg:ml-sidebar-width w-full flex-1 p-2 md:p-6">
-        {children}
-      </section>
-    </main>
-  );
+  return <DocsLayoutShell>{children}</DocsLayoutShell>;
 };
 
 export default ComponentLayout;
