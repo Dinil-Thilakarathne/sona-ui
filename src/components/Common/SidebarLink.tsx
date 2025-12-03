0ab94b19eaa5c6e2954a3d02f4ae26df13de53e9
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-12-03 07:37:15 +0530

    fix: update sidebar ui

--- src/components/Common/SidebarLink.tsx
+++ src/components/Common/SidebarLink.tsx
@@ -31,7 +31,7 @@ const SidebarLink = ({
       href={href}
       {...props}
     >
-      <h3 className={cn("text-sm text-muted-foreground", textClassName)}>{name}</h3>
+      <h3 className={cn("text-sm text-muted-foreground group-hover:text-foreground", textClassName)}>{name}</h3>
       {tag && <Tag text={tag} type={tag} className="px-1 py-0 text-xs" />}
       <div className="bg-foreground absolute -bottom-0.5 left-0 h-0.5 w-0 transition-[width] duration-300 group-hover:w-full group-data-[active=true]:w-full"></div>
     </Link>
